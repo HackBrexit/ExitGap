d87553f5a0111fd6fe859a62cdf7b5e951bc0127
esbaddeley <esbaddeley@gmail.com>
2016-07-24 12:22:36 +0100

    Added location prop to the button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ import classes from './Button.scss';
 export const Button = (props) => {
   return (
       <div>
-        <Link to={"/" + props.name}>
+        <Link to={"/" + props.location}>
           <div>{props.name}</div>
         </Link>
       </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Button from '../../components/Button/index';
 
-const List = [{name: "Funding and Finance"}, {name: "Policy and Regulation"}, {name: "Trade"} ]
+const List = [{name: "Funding and Finance", location: "FundingAndFinance"}, {name: "Policy and Regulation", location: "PolicyAndRegulation"}, {name: "Trade", location: "Trade"} ]
+
 
 class Home extends Component {
 
+  createLocation(name){
+    return name;
+  };
+
   renderButtons(){
     return List.map(item => {
-      return (
-        <li key={item.name}>
-          <Button name={item.name} />
-        </li>
+        return (
+          <li key={item.name}>
+            <Button name={item.name} location={item.location} />
+          </li>
       );
     });
   };
+
 
   render() {
     return (
